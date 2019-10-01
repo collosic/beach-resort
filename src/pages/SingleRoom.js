6996dc57c31ef92b53563909337f6a61e585ba0a
christian <christian.collosi@gmail.com>
2019-10-01 09:23:47 -0700

    Set up react router and basic navbar. Add in data and images for setup purposes

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>Hello from Single Room</div>;
+  }
+}
+
+export default SingleRoom;
